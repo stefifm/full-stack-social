9ee839e499a7a10cdac1cd3e6dcab69c3fe6c0d5
Stefania Veronica Bruera <59149@sistemas.frc.utn.edu.ar>
2023-12-11 13:25:08 -0300

    Add image

--- client/src/components/LeftBar/LeftBar.jsx
+++ client/src/components/LeftBar/LeftBar.jsx
@@ -1,7 +1,96 @@
+import Friends from '../../assets/1.png'
+import Groups from '../../assets/2.png'
+import Market from '../../assets/3.png'
+import Watch from '../../assets/4.png'
+import Memories from '../../assets/5.png'
+import Events from '../../assets/6.png'
+import Gaming from '../../assets/7.png'
+import Gallery from '../../assets/8.png'
+import Videos from '../../assets/9.png'
+import Messages from '../../assets/10.png'
+import Tutorials from '../../assets/11.png'
+import Courses from '../../assets/12.png'
+import Fund from '../../assets/13.png'
 import './leftbar.scss'
 
 const LeftBar = () => {
-  return <div className='leftbar'>LeftBar</div>
+  const firstSection = [
+    { name: 'Friends', icon: Friends },
+    { name: 'Groups', icon: Groups },
+    { name: 'Marketplace', icon: Market },
+    { name: 'Watch', icon: Watch },
+    { name: 'Memories', icon: Memories }
+  ]
+
+  const secondSection = [
+    { name: 'Events', icon: Events },
+    { name: 'Gaming', icon: Gaming },
+    { name: 'Gallery', icon: Gallery },
+    { name: 'Videos', icon: Videos },
+    { name: 'Messages', icon: Messages }
+  ]
+
+  const thirdSection = [
+    { name: 'Fundraisers', icon: Fund },
+    { name: 'Tutorials', icon: Tutorials },
+    { name: 'Courses', icon: Courses }
+  ]
+  return (
+    <aside className='leftbar'>
+      <div className='container'>
+        <div className='menu'>
+          <div className='user'>
+            <img
+              src='https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600'
+              alt=''
+            />
+            <span>Mark Kansas</span>
+          </div>
+          {firstSection.map((item, index) => (
+            <div
+              className='item'
+              key={index}>
+              <img
+                src={item.icon}
+                alt=''
+              />
+              <span>{item.name}</span>
+            </div>
+          ))}
+        </div>
+        <hr />
+        <div className='menu'>
+          <span>Your shortcuts</span>
+          {secondSection.map((item, index) => (
+            <div
+              className='item'
+              key={index}>
+              <img
+                src={item.icon}
+                alt=''
+              />
+              <span>{item.name}</span>
+            </div>
+          ))}
+        </div>
+        <hr />
+        <div className='menu'>
+          <span>Others</span>
+          {thirdSection.map((item, index) => (
+            <div
+              className='item'
+              key={index}>
+              <img
+                src={item.icon}
+                alt=''
+              />
+              <span>{item.name}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </aside>
+  )
 }
 
 export default LeftBar
--- client/src/components/RightBar/RightBar.jsx
+++ client/src/components/RightBar/RightBar.jsx
@@ -1,7 +1,42 @@
 import './rightbar.scss'
 
 const RightBar = () => {
-  return <div className='rightbar'>RightBar</div>
+  const userInfo = [
+    {
+      img: 'https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      name: 'William Doe'
+    },
+    {
+      img: 'https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      name: 'John Doe'
+    }
+  ]
+  return (
+    <aside className='rightbar'>
+      <div className='container'>
+        <div className='item'>
+          <span>Suggestions For You</span>
+          {userInfo.map((user, index) => (
+            <div
+              className='user'
+              key={index}>
+              <div className='userInfo'>
+                <img
+                  src={user.img}
+                  alt=''
+                />
+                <span>{user.name}</span>
+              </div>
+              <div className='btn-group'>
+                <button>Follow</button>
+                <button>Dismiss</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </aside>
+  )
 }
 
 export default RightBar
