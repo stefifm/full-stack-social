c6228618af81cf3f61847778cee577e9ba87aa74
Stefania Veronica Bruera <59149@sistemas.frc.utn.edu.ar>
2023-12-11 13:24:46 -0300

    Change current user condition

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import Home from './pages/home/Home'
 import Profile from './pages/profile/Profile'
 
 function App() {
-  const currentUser = false
+  const currentUser = true
 
   const Layout = () => {
     return (
@@ -17,7 +17,9 @@ function App() {
         <Navbar />
         <div style={{ display: 'flex' }}>
           <LeftBar />
-          <Outlet />
+          <div style={{ flex: 6 }}>
+            <Outlet />
+          </div>
           <RightBar />
         </div>
       </div>
