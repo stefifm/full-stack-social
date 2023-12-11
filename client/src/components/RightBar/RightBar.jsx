2cd8136f2f843b4d77241d24aefd46fd440708b7
Stefania Veronica Bruera <59149@sistemas.frc.utn.edu.ar>
2023-12-11 19:47:53 -0300

    Finnish with right bar

--- client/src/components/RightBar/RightBar.jsx
+++ client/src/components/RightBar/RightBar.jsx
@@ -11,6 +11,70 @@ const RightBar = () => {
       name: 'John Doe'
     }
   ]
+
+  const latestActivities = [
+    {
+      img: 'https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      name: 'William Doe',
+      action: 'liked your post',
+      date: '1 min ago'
+    },
+    {
+      img: 'https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      name: 'William Doe',
+      action: 'liked your post',
+      date: '1 min ago'
+    },
+    {
+      img: 'https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      name: 'William Doe',
+      action: 'liked your post',
+      date: '1 min ago'
+    },
+    {
+      img: 'https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      name: 'William Doe',
+      action: 'liked your post',
+      date: '1 min ago'
+    },
+    {
+      img: 'https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      name: 'John Doe',
+      action: 'commented on your post',
+      date: '2 min ago'
+    }
+  ]
+
+  const onlineFriends = [
+    {
+      img: 'https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      name: 'William Doe'
+    },
+    {
+      img: 'https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      name: 'William Doe'
+    },
+    {
+      img: 'https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      name: 'William Doe'
+    },
+    {
+      img: 'https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      name: 'William Doe'
+    },
+    {
+      img: 'https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      name: 'William Doe'
+    },
+    {
+      img: 'https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      name: 'William Doe'
+    },
+    {
+      img: 'https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      name: 'William Doe'
+    }
+  ]
   return (
     <aside className='rightbar'>
       <div className='container'>
@@ -30,6 +94,42 @@ const RightBar = () => {
               <div className='btn-group'>
                 <button>Follow</button>
                 <button>Dismiss</button>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className='item'>
+          <span>Latest Activities</span>
+          {latestActivities.map((activity, index) => (
+            <div
+              className='user'
+              key={index}>
+              <div className='userInfo'>
+                <img
+                  src={activity.img}
+                  alt=''
+                />
+                <p>
+                  <span>{activity.name}</span> {activity.action}
+                </p>
+              </div>
+              <span>{activity.date}</span>
+            </div>
+          ))}
+        </div>
+        <div className='item'>
+          <span>Online Friends</span>
+          {onlineFriends.map((friend, index) => (
+            <div
+              className='user'
+              key={index}>
+              <div className='userInfo'>
+                <img
+                  src={friend.img}
+                  alt=''
+                />
+                <div className='online' />
+                <span>{friend.name}</span>
               </div>
             </div>
           ))}
