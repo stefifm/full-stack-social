a85f3ef4050e9900274dcbeaa2c7b0a6d5fca4b0
Stefania Veronica Bruera <59149@sistemas.frc.utn.edu.ar>
2023-12-13 12:19:39 -0300

    Fix issue for small mobiles

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import Stories from '../../components/Stories/Stories'
 import Posts from '../../components/Posts/Posts'
 import './home.scss'
+import Share from '../../components/Share/Share'
 const Home = () => {
   return (
     <main className='home'>
       <Stories />
+      <Share />
       <Posts />
     </main>
   )
